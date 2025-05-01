2be1c1a2066ad93f8d036720e5cd769b5c686a97
Lzhenghua <455314714@qq.com>
2025-05-02 06:54:22 +0800

    Update CosineSimilarity.py

---
+++ ClusterKT/similarity_metric/CosineSimilarity.py
@@ -0,0 +1,6 @@
+class CosineSimilarity(nn.Module):
+ 
+    def forward(self, tensor_1, tensor_2):
+        normalized_tensor_1 = tensor_1 / tensor_1.norm(dim=-1, keepdim=True)
+        normalized_tensor_2 = tensor_2 / tensor_2.norm(dim=-1, keepdim=True)
+        return (normalized_tensor_1 * normalized_tensor_2).sum(dim=-1)

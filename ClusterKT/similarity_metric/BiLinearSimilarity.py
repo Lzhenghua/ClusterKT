e65581ef988d14f22b28af223f4995c7a9dc2480
Lzhenghua <455314714@qq.com>
2025-05-02 06:55:42 +0800

    Update BiLinearSimilarity.py

---
+++ ClusterKT/similarity_metric/BiLinearSimilarity.py
@@ -0,0 +1,19 @@
+class BiLinearSimilarity(nn.Module):
+ 
+    def __init__(self, tensor_1_dim, tensor_2_dim, activation=None):
+        super(BiLinearSimilarity, self).__init__()
+        self.weight_matrix = nn.Parameter(torch.Tensor(tensor_1_dim, tensor_2_dim))
+        self.bias = nn.Parameter(torch.Tensor(1))
+        self.activation = activation
+        self.reset_parameters()
+ 
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.weight_matrix)
+        self.bias.data.fill_(0)
+ 
+    def forward(self, tensor_1, tensor_2):
+        intermediate = torch.matmul(tensor_1, self.weight_matrix)
+        result = (intermediate * tensor_2).sum(dim=-1) + self.bias
+        if self.activation is not None:
+            result = self.activation(result)
+        return result
